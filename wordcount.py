6f95a630aa2692ad5077984d4f2a1e1fb8c91ba3
Your Name <mahafatima14@gmail.com>
2021-10-27 03:33:00 +0000

    Futher studies data cleansing

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,5 @@
+import re
+
 def get_word_count_from_poem(poem_file):
     """Count words in file."""
 
@@ -9,9 +11,11 @@ def get_word_count_from_poem(poem_file):
         line = line.rstrip()                            #remove right white space
         word_list = line.split(" ")                  #split each line by |
     
+        for i, word in enumerate(word_list):
+            word_list[i] = re.sub('[^\w+]', '', word_list[i]).lower()
+
         for word in word_list:
             poem_dict[word] = poem_dict.get(word, 0) + 1
-
     
     for word, count in poem_dict.items():
         print(f'{word}: {count}')
@@ -21,3 +25,11 @@ def get_word_count_from_poem(poem_file):
 
 get_word_count_from_poem("twain.txt")
 
+
+
+
+# >>> # Find the ten most common words in Hamlet
+# >>> import re
+# >>> words = re.findall(r'\w+', open('hamlet.txt').read().lower())
+# >>> Counter(words).most_common(10)
+# [('the', 1143), ('and', 966), ('to', 762)
